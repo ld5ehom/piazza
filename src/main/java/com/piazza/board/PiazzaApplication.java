e18f6f5c9c1fb5997c3a0676301c9f53806926f4
TaeWook Park <ld5ehom@gmail.com>
2024-07-18 18:13:56 -0700

    pizza project init

---
+++ src/main/java/com/piazza/board/PiazzaApplication.java
@@ -0,0 +1,13 @@
+package com.piazza.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiazzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiazzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piazza/board/PiazzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piazza.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiazzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
